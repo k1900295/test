b36026db24a653e67f62ee3e20a351887bc8bd08
k1900295 <57809159+k1900295@users.noreply.github.com>
2019-11-22 15:05:29 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TaulukkoTesti1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luvut = new int[5];
+
+            luvut[0] = 2;
+
+            for (int i = 1; i < luvut.Length; i++)
+            {
+                luvut[i] = i + 6;
+            }
+
+            for (int i = 0; i < luvut.Length; i++)
+            {
+                Console.Write(luvut[i] + " ");
+            }
+            Console.WriteLine();
+
+            foreach (int item in luvut)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
